7fe80f148cbdd5791ca10b4904e0def0af125b58
gdm-1718-saravand46 <sarah.vandenberghe2@student.arteveldehs.be>
2018-04-16 07:37:04 +0200

    verdere bewerking

--- assets/scripts/navbar.js
+++ assets/scripts/navbar.js
@@ -8,22 +8,33 @@ $(document).ready(function(){
        scroll_start = $(this).scrollTop();
        if(scroll_start > offset.top) { // bij het scrollen
            $(".navbar").css('background-color', '#fff');
-           $(".navbar").css('box-shadow', '0 0px 50px 0 rgba(0, 0, 0, 0.2)');
+           $(".navbar").css('box-shadow', '0 0 50px 0 rgba(0, 0, 0, 0.2)');
            $(".navbar a").css('color', '#000');
-           $("#download-pdf").css('background-color', 'rgba(255,153,0,1)');
-           $("#download-pdf").css('color', '#fff');
-           $(".navbar a:hover").css('color', 'rgba(255,153,0,1)'); // werkt niet ?
-           $(".active > a").css('color', 'rgba(255,153,0,1)');
+           $("#download-pdf").css('background-color', 'rgba(255,153,0,1)'); // oranje
 
+           $("#download-pdf").mouseenter(function(){
+                $("#download-pdf").css("background-color", "rgba(0,153,255,1)"); });
+            $("#download-pdf").mouseleave(function(){
+                 $("#download-pdf").css("background-color", "rgba(255,153,0,1)"); });
+
+           $("#download-pdf").css('color', '#fff');
+           $(".active > a").css('color', 'rgba(255,153,0,1)');
+           $('.navbar .navbar-brand img').attr('src','/1718-3CMO-BaP-saravand46/assets/images/logo-woordmerk.png')
+
+
+           
         } else { //helemaal bovenaan
            $(".navbar").css('background-color', 'transparent');
-           $(".navbar").css('box-shadow', '0 0px 50px 0 rgba(0, 0, 0, 0.0)');
+           $(".navbar").css('box-shadow', '0 0 0 0 rgba(0, 0, 0, 0.0)');
            $(".navbar a").css('color', '#fff');
-           $("#download-pdf").css('background-color', '#fff');
-           $("#download-pdf").css('color', 'rgba(255,153,0,1)');
-           $(".navbar a:hover").css('color', '#000'); // werkt niet ? --> opgelost maar niet via hier.
-           $(".active > a:active").css('color', '#000'); // nope
+           $("#download-pdf").css('background-color', 'rgba(0,153,255,1)'); // blauw
+           $("#download-pdf").css('color', '#fff');
+           $('.navbar .navbar-brand img').attr('src','/1718-3CMO-BaP-saravand46/assets/images/logo-mono-wit-woordmerk.png')
         }
     });
      }
  });
+
+ $(document).ready(function() {
+    $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+});
